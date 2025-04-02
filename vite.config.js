201b2ea119f0d87eaff9d6959ec38318b1411a3c
Vishal D. <148406419+Hummingcoder@users.noreply.github.com>
2025-04-02 12:02:13 +0530

    yeah

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/interactive-comment-section",
+  base: "/COMMENT_SECTION",
   plugins: [tailwindcss(), react()],
 });
